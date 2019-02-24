38c586c3ad64ca7809722631717ee3652ef6e71a
Nguyen Truong Giang <nguyentruonggiang91@gmail.com>
2019-02-24 20:44:58 +0700

    + update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,7 +48,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        //return parent::render($request, $exception);
+        if(app()->isLocal()){
+            return parent::render($request, $exception);
+        }
+        
         return response()->json(
             [
                 'error' => $exception->getMessage(),
